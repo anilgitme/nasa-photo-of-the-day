893c795c5960ab26e273f5a9827a61fa46f768e3
Anil_Ramcharran <anil.gitme@gmail.com>
2020-11-18 00:06:43 -0500

    need to finish i exceeded the get request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+//3 slices of state imgs/title and info
+const [image, setImage] =  useState('');
+const [title, setTitle] = useState('')
+const [info, setInfo] = useState('')
+
+//get request
+const getData = () => {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=PM7diFdO6ecLlEHfJ6XU1KZw9zbc36IGWNPNphIq&date=2020-11-17')
+  .then(res => {
+  console.log(res)
+  setImage(res.data.hdurl)
+  setTitle(res.data.title)
+  setInfo(res.data.explaination)
+  })
+  .catch(err => console.log(err))
+}
+useEffect(() => getData(), [])
+
+
   return (
     <div className="App">
       <p>
